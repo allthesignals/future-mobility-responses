2f9e171f79ef9fa703b5b05e03342a408b084425
allthesignals <wilburnforce@gmail.com>
2018-11-19 09:53:52 -0600

    Update host and put it into environmentjs

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,9 +1,12 @@
 import Route from '@ember/routing/route';
 import fetch from 'fetch';
+import environment from '../config/environment';
+
+const { host } = environment;
 
 export default class ApplicationRoute extends Route {
   model() {
-    return fetch('http://18.85.45.120/qcount/?format=json')
+    return fetch(`${host}/qcount/?format=json`)
       .then(blob => blob.json());
   }
 }
